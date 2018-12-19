62f2bf2a290184c3935c99a72e66f3c9e82d9f38
ryotaro <ryo.cv.0311@gmail.com>
2018-12-19 15:58:28 +0900

    done

--- generate_dataset.py
+++ generate_dataset.py
@@ -6,8 +6,8 @@ import datetime
 
 import cv2
 import joblib
-
 from pytube import YouTube
+from time import sleep
 
 class Data:
     def __init__(self, url, seqname, list_timestamps):
@@ -24,6 +24,43 @@ class Data:
 
     def __len__(self):
         return len(self.list_seqnames)
+
+
+def process(data, seq_id, videoname, output_root):
+    seqname = data.list_seqnames[seq_id]
+    if not os.path.exists(output_root + seqname):
+        os.makedirs(output_root + seqname)
+    else:
+        print("[INFO] Something Wrong, stop process")
+        return True
+
+    list_str_timestamps = []
+    for timestamp in data.list_list_timestamps[seq_id]:
+        timestamp = int(timestamp/1000) 
+        str_hour = str(int(timestamp/3600000)).zfill(2)
+        str_min = str(int(int(timestamp%3600000)/60000)).zfill(2)
+        str_sec = str(int(int(int(timestamp%3600000)%60000)/1000)).zfill(2)
+        str_mill = str(int(int(int(timestamp%3600000)%60000)%1000)).zfill(3)
+        _str_timestamp = str_hour+":"+str_min+":"+str_sec+"."+str_mill
+        list_str_timestamps.append(_str_timestamp)
+
+    # extract frames from a video
+    for idx, str_timestamp in enumerate(list_str_timestamps):
+        command = 'ffmpeg -ss '+str_timestamp+' -i '+videoname+' -vframes 1 -f image2 '+output_root+seqname+'/'+str(data.list_list_timestamps[seq_id][idx])+'.png'
+        # print("current command is {}".format(command))
+        os.system(command)
+
+    png_list = glob.glob(output_root+"/"+seqname+"/*.png")
+
+    for pngname in png_list:
+        img = cv2.imread(pngname, 1)
+        if int(img.shape[1]/2) < 500:
+            break
+        img = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
+        cv2.imwrite(pngname, img)
+
+    return False
+
 
 class DataDownloader:
     def __init__ (self, dataroot, mode='test'):
@@ -70,47 +107,16 @@ class DataDownloader:
                 if not isRegistered:
                     self.list_data.append(Data(youtube_url, seq_name, list_timestamps))
 
+            # self.list_data.reverse()
             print(" Done! ")
             print("[INFO] {} movies are used in {} mode".format(len(self.list_data), self.mode))
-
-    def process(self, data, seq_id, videoname):
-        seqname = data.list_seqnames[seq_id]
-        if not os.path.exists(self.output_root + seqname):
-            os.makedirs(self.output_root + seqname)
-        else:
-            print("[INFO] Something Wrong, stop process")
-            self.isDone = True
-            return False
-
-        list_str_timestamps = []
-        for timestamp in data.list_list_timestamps[seq_id]:
-            timestamp = int(timestamp/1000) 
-            str_hour = str(int(timestamp/3600000)).zfill(2)
-            str_min = str(int(int(timestamp%3600000)/60000)).zfill(2)
-            str_sec = str(int(int(int(timestamp%3600000)%60000)/1000)).zfill(2)
-            str_mill = str(int(int(int(timestamp%3600000)%60000)%1000)).zfill(3)
-            _str_timestamp = str_hour+":"+str_min+":"+str_sec+"."+str_mill
-            list_str_timestamps.append(_str_timestamp)
-
-        # extract frames from a video
-        for idx, str_timestamp in enumerate(list_str_timestamps):
-            command = 'ffmpeg -ss '+str_timestamp+' -i '+videoname+' -vframes 1 -f image2 '+self.output_root+'/'+seqname+'/'+str(data.list_list_timestamps[seq_id][idx])+'.png'
-            os.system(command)
-
-        png_list = glob.glob(self.output_root+"/"+seqname+"/*.png")
-
-        for pngname in png_list:
-            img = cv2.imread(pngname, 1)
-            if int(img.shape[1]/2) < 500:
-                break
-            img = cv2.resize(img, (int(img.shape[1]/2), int(img.shape[0]/2)))
-            cv2.imwrite(pngname, img)
 
 
     def Run(self):
         print("[INFO] Start downloading {} movies".format(len(self.list_data)))
 
         for global_count, data in enumerate(self.list_data):
+            print("[INFO] Downloading {} ".format(data.url))
             try :
                 # sometimes this fails because of known to issues of pytube and unknown factors
                 yt = YouTube(data.url)
@@ -123,21 +129,31 @@ class DataDownloader:
                 failure_log.close()
                 continue
 
+            # cv2.waitKey(1500)
+            sleep(1)
+
             videoname_candinate_list = glob.glob('./*')
             for videoname_candinate in videoname_candinate_list:
                 if videoname_candinate.split('.')[-2] == '/current_'+mode:
                     videoname = videoname_candinate
 
-            # this loop can be done in multiprocess or threads
-
             if len(data) == 1: # len(data) is len(data.list_seqnames)
-                process(data, 0, videoname)
+                flag = process(data, 0, videoname, self.output_root)
+                list_flags = []
+                list_flags.append(flag)
             else:
-                joblib.Parallel(n_jobs=4)([joblib.delayed(self.process)(data, seq_id, videoname) for seq_id in range(len(data))])
+                list_flags = joblib.Parallel(n_jobs=4,backend="multiprocessing")([joblib.delayed(process)(data, seq_id, videoname, self.output_root) for seq_id in range(len(data))])
 
             # remove videos
             command = "rm " + videoname 
             os.system(command)
+
+            for flag in list_flags:
+                if flag:
+                    self.isDone =  True
+
+            if self.isDone:
+                return False
 
         return True
 
